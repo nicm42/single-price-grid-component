1b430b40c6567fb0397a3bcfd76192e8e0d4b94f
Nic Mayer <nicmayer42@gmail.com>
2020-05-08 17:10:57 +0100

    Done the colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cyan': 'hsl(179, 62%, 43%)',
+        'brightYellow': 'hsl(71, 73%, 54%)',
+        'lightGrey': 'hsl(204, 43%, 93%)',
+        'greyishBlue': 'hsl(218, 22%, 67%)'
+      },
+      fontFamily: {
+        'sans': ['Karla', 'sans-serif']
+      }
+    },
   },
   variants: {},
   plugins: [],
